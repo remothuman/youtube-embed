f69eda13b45530baf58d665ec5e5af7152077658
tomer <101682824+tomernahum@users.noreply.github.com>
2026-01-18 08:13:18 -0500

    redirect /shorts

---
+++ src/routes/shorts/[v]/+page.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+    import { page } from '$app/stores';
+    import { goto } from '$app/navigation';
+    import { browser } from '$app/environment';
+    let {v} = $page.params;
+
+    if (browser) {
+        goto(`/watch?v=${v}`, { replaceState: true });
+    }
+</script>
+<p>Redirecting…</p>
