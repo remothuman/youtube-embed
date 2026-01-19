9c694ff4057eb86ffce76f9cfe39e64b7d072ce3
tomer <101682824+tomernahum@users.noreply.github.com>
2026-01-19 01:02:33 -0500

    update progress etc

--- development/ralph-once.sh
+++ development/ralph-once.sh
@@ -1,12 +1,10 @@
 #!/bin/bash
 
-claude2 --permission-mode acceptEdits "@PRD
-.md @progress
-.txt @plan \\
-.md\\
+
+CLAUDE_CONFIG_DIR=~/.claude2 claude --permission-mode acceptEdits "@development/PRD.json @development/plan.md @development/progress.txt
 1. Read the PRD and plan and progress files. \\
 2. Find the next incomplete task and implement it. \\
-3. run a code review subagent to review the changes, fix any issues oyu agree with.
-4. Commit your changes. \\
-5. Update progress.txt with what you did. \\
+3. run a code review subagent to review the changes, fix any issues you agree with.
+4. Update progress.txt with what you did.\\
+5. Commit your changes. \\
 only do one thing at a time. if the prd is 100% complete, output <promise>COMPLETE</promise> "
